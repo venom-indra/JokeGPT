2bc218c65e85d921b80e718e9dddc930b017d255
INDRA KUMAR R <110984003+venom-indra@users.noreply.github.com>
2023-08-22 18:10:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?type=single";
+
+let getJoke = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      jokeContainer.textContent = `${item.joke}`;
+    });
+};
+btn.addEventListener("click", getJoke);
+getJoke();
+
+const spanElement = document.querySelector("span");
+
+function startRainbowAnimation() {
+  spanElement.classList.add("rainbow-text");
+}
+
+startRainbowAnimation();
